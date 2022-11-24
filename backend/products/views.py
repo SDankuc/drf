7f2e2100ba8fef1dd2e35bd22d9f7d1edd856913
Stefan <stefan.dankuc.dev@gmail.com>
2022-11-24 16:09:10 +0100

    Django Rest Framework ListAPIView

--- backend/products/views.py
+++ backend/products/views.py
@@ -4,7 +4,7 @@ from rest_framework import generics
 from .models import Product
 from.serializers import ProductSerializer
 
-class ProductCreateAPIView(generics.CreateAPIView):
+class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
@@ -18,9 +18,20 @@ class ProductCreateAPIView(generics.CreateAPIView):
         serializer.save(content=content)
         # send a Django signal <- this can be done here
 
-product_create_view = ProductCreateAPIView.as_view()
+product_list_create_view = ProductListCreateAPIView.as_view()
 
 class ProductDetailApiView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     #lookup_field = "pk"
+
+#not using this method -> ProductCreateApiView changing to ProductListCreateApiView
+class ProductListApiView(generics.ListAPIView):
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    #lookup_field = "pk"
+
+
+
+
