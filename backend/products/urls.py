790dccb9269551b7a7df4fa194e551f8ad700aab
Stefan <stefan.dankuc.dev@gmail.com>
2022-11-24 17:19:19 +0100

    Mixins and a Generic API View

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.product_list_create_view), 
+    path("", views.ProductMixinView.as_view()), 
     path("<int:pk>/update/", views.ProductUpdateApiView.as_view()),
     path("<int:pk>/delete/", views.ProductDeleteApiView.as_view()),
-    path("<int:pk>/", views.ProductDetailApiView.as_view()),
+    path("<int:pk>/", views.ProductMixinView.as_view()),
 ]
