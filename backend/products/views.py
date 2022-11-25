8c3dfc191b7e047807133dc1fb126af22b270e15
Stefan <stefan.dankuc.dev@gmail.com>
2022-11-25 09:36:29 +0100

    Custom Permissions

--- backend/products/views.py
+++ backend/products/views.py
@@ -6,13 +6,14 @@ from django.shortcuts import get_object_or_404
 
 
 from .models import Product
+from .permissions import IsStaffEditorPermissions
 from.serializers import ProductSerializer
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     authentication_classes = [authentication.SessionAuthentication]
-    permission_classes = [permissions.DjangoModelPermissions]
+    permission_classes = [permissions.IsAdminUser, IsStaffEditorPermissions]
 
     def perform_create(self, serializer):
         #serializer.save(user=self.request.user)
