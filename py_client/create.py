a01cb7be0bbb5288b21a1737f9c919172f1c382b
Stefan <stefan.dankuc.dev@gmail.com>
2022-11-25 10:24:56 +0100

    Token Authentication

--- py_client/create.py
+++ py_client/create.py
@@ -1,12 +1,14 @@
 import requests
 
+headers = {
+    "Authorization":"Bearer86396d4d30ca82619910b2f848b6fb6ee6362df8"
+    }
+
 endpoint = "http://localhost:8000/api/products/"
 
 data = {
     "title": "this field is done",
     "price": 32.99
 }
-
 get_response = requests.post(endpoint, json=data)
-
 print(get_response.json())
