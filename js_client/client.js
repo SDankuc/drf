4b8c51605c53e748a9c35d15fa9497c54501f2b6
Stefan <stefan.dankuc.dev@gmail.com>
2022-11-29 13:20:47 +0100

    Search via REST API & JS Client

--- js_client/client.js
+++ js_client/client.js
@@ -1,10 +1,17 @@
 const contentContainer = document.getElementById('content-container')
 const loginForm = document.getElementById('login-form')
+const searchForm = document.getElementById('search-form')
 const baseEndpoint = "http://localhost:8000/api"
 if (loginForm){
     //handle this login form
     loginForm.addEventListener('submit', handleLogin)
 }
+
+if (searchForm){
+    //handle this login form
+    searchForm.addEventListener('submit', handleSearch)
+}
+
 
 function writeToContainer(data){
     if (contentContainer){
@@ -56,6 +63,37 @@ function handleLogin(event){
     })
 }
 
+function handleSearch(event){
+    event.preventDefault()
+
+    let formData = new FormData(searchForm)
+    let data = Object.fromEntries(formData)
+    let searchParams = new URLSearchParams(data)
+
+    const endpoint = `${baseEndpoint}/search/?${searchParams}`
+    const headers = {
+        "Content-Type":"application/json",
+    }
+    const authToken = localStorage.getItem('access')
+    if (authToken){
+        headers['Authorization'] = `Bearer ${authToken}`
+    }
+    const options = {
+        method:"GET",
+        headers:headers
+    }
+    fetch(endpoint, options) //  Promise
+    .then(response=>{
+        return response.json()
+    })
+    .then(data => {
+        writeToContainer(data)
+    })
+    .catch(err =>{
+        console.log("err",err)
+    })
+}
+
 function handleAuthData(authData, callback){
     localStorage.setItem('access', authData.access)
     localStorage.setItem('refresh', authData.refresh)
@@ -80,4 +118,22 @@ function getProductList(){
     })
 }
 
+function validateJWTToken(){
+    const endpoint = `${baseEndpoint}/token/verify/`
+    const options = {
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify({
+            token: localStorage.getItem('access')
+        })
+    }
+    fetch(endpoint,options)
+    .then(response=> response.json())
+    .then(x=>{
+        // refresh token
+    })
+}
+validateJWTToken()
 getProductList()
