1a247b0697b492ba3209f78d62272d982576ed7e
Stefan <stefan.dankuc.dev@gmail.com>
2022-11-29 13:01:05 +0100

    Using JWT with JS Client

--- js_client/client.js
+++ js_client/client.js
@@ -1,3 +1,4 @@
+const contentContainer = document.getElementById('content-container')
 const loginForm = document.getElementById('login-form')
 const baseEndpoint = "http://localhost:8000/api"
 if (loginForm){
@@ -5,8 +6,32 @@ if (loginForm){
     loginForm.addEventListener('submit', handleLogin)
 }
 
+function writeToContainer(data){
+    if (contentContainer){
+        contentContainer.innerHTML = "<pre>" + JSON.stringify(data, null, 4) + "</pre>"
+    }
+}
+
+function getFetchOptions(method, body){
+    return {
+        method: method === null?"GET" : method,
+        headers:{
+            "Content-Type":"application/json",
+            "Authorization": `Bearer ${localStorage.getItem('access')}`
+        },
+        body: body ? body : null
+    }
+}
+
+function isTokenNotValid(jsonData){
+    if(jsonData.code && jsonData.code === "token_not_valid"){
+        alert("Please login again")
+        return false
+    }
+    return true
+}
+
 function handleLogin(event){
-    console.log(event)
     event.preventDefault()
     const loginEndpoint = `${baseEndpoint}/token/`
     let loginFormData = new FormData(loginForm)
@@ -23,10 +48,36 @@ function handleLogin(event){
     .then(response=>{
         return response.json()
     })
-    .then(x =>{
-        console.log(x)
+    .then(authData => {
+        handleAuthData(authData, getProductList)
     })
     .catch(err =>{
         console.log("err",err)
     })
 }
+
+function handleAuthData(authData, callback){
+    localStorage.setItem('access', authData.access)
+    localStorage.setItem('refresh', authData.refresh)
+    if (callback){
+        callback()
+    }
+}
+
+function getProductList(){
+    const endpoint = `${baseEndpoint}/products/`
+    const options = getFetchOptions()
+    fetch(endpoint,options)
+    .then(response =>{
+        return response.json()
+    })
+    .then(data=>{
+        const validData = isTokenNotValid(data)
+        if (validData){
+            writeToContainer(data)
+        }
+
+    })
+}
+
+getProductList()
