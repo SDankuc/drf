26f206d964dd00dbceeab9bbcdf8622c7d45e663
Stefan <stefan.dankuc.dev@gmail.com>
2022-11-29 10:01:16 +0100

    JSON WEB Token Authentication with simplejwt

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from . import views
 
 urlpatterns = [
     path("auth/", obtain_auth_token),
+    path("token/", TokenObtainPairView.as_view(), name = 'token_obtain_pair'), # api/token
+    path("token/", TokenRefreshView.as_view(), name = "token_refresh"),
+    path("token/", TokenVerifyView.as_view(), name = "token_verify"),
     path("",views.api_home)
 ]
